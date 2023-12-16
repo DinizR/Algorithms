b56b5ee079938e1e6690310bdc664e0d5b9cdfad
rodrigo.dinis <rodrigo.dinis@outlook.com>
2023-12-16 23:29:14 +1300

    Implementing ArrayList

---
+++ Arrays/Arrays/ArrayList.h
@@ -0,0 +1,37 @@
+//
+//  ArrayList.h
+//  Arrays
+//
+//  Created by Rodrigo Dinis on 16/12/23.
+//
+
+#ifndef ArrayList_h
+#define ArrayList_h
+
+#include <iostream>
+
+template <class T>
+class ArrayList {
+private:
+    static const int DEFAULT_CAPACITY = 10;
+    T *array;
+    int size;
+    int capacity;
+    void swapToNew();
+    
+public:
+    ArrayList();
+    ArrayList(int initialCapacity);
+    ~ArrayList();
+    
+    void add(T value);
+    void remove(int index);
+    bool contains(T value);
+    T get(int index);
+    int getSize();
+    int getCapacity();
+};
+
+#include "ArrayList_Impl.h"
+
+#endif /* ArrayList_h */
